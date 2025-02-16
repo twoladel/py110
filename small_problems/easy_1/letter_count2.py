1decaec3bc6c0d1cdfa158b0ff740dfc45dbd4f7
Joel Turske <joelturske@gmail.com>
2025-02-15 22:49:16 -0500

    wrapped it in a function to resolve pylint issues.

--- small_problems/easy_1/letter_count2.py
+++ small_problems/easy_1/letter_count2.py
@@ -11,21 +11,23 @@ def word_sizes(str1):
 
         words.setdefault(len(clean_word), 0)
         words[len(clean_word)] += 1
-    
+
     return words
 
 # All of these examples should print True
+def run_program():
+    string = 'Four score and seven.'
+    print(word_sizes(string) == {4: 1, 5: 2, 3: 1})
 
-string = 'Four score and seven.'
-print(word_sizes(string) == {4: 1, 5: 2, 3: 1})
+    string = 'Hey diddle diddle, the cat and the fiddle!'
+    print(word_sizes(string) == {3: 5, 6: 3})
 
-string = 'Hey diddle diddle, the cat and the fiddle!'
-print(word_sizes(string) == {3: 5, 6: 3})
+    string = 'Humpty Dumpty sat on a w@ll'
+    print(word_sizes(string) == {6: 2, 3: 2, 2: 1, 1: 1})
 
-string = 'Humpty Dumpty sat on a w@ll'
-print(word_sizes(string) == {6: 2, 3: 2, 2: 1, 1: 1})
+    string = "What's up doc?"
+    print(word_sizes(string) == {5: 1, 2: 1, 3: 1})
 
-string = "What's up doc?"
-print(word_sizes(string) == {5: 1, 2: 1, 3: 1})
+    print(word_sizes('') == {})
 
-print(word_sizes('') == {})
+run_program()
