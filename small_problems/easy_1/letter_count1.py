5721ebb10c4da7a73296d1819292f40af54e66dd
Joel Turske <joelturske@gmail.com>
2025-02-15 22:36:07 -0500

    letter count part 1

---
+++ small_problems/easy_1/letter_count1.py
@@ -0,0 +1,26 @@
+def word_sizes(str1):
+    words = {}
+    str1 = str1.split()
+    for word in str1:
+        words.setdefault(len(word), 0)
+        words[len(word)] += 1
+    
+    return words
+
+# All of these examples should print True
+
+string = 'Four score and seven.'
+print(word_sizes(string) == {4: 1, 5: 1, 3: 1, 6: 1})
+
+string = 'Hey diddle diddle, the cat and the fiddle!'
+print(word_sizes(string) == {3: 5, 6: 1, 7: 2})
+
+string = 'Humpty Dumpty sat on a wall'
+print(word_sizes(string) == {6: 2, 3: 1, 2: 1, 1: 1, 4: 1})
+
+string = "What's up doc?"
+print(word_sizes(string) == {6: 1, 2: 1, 4: 1})
+
+print(word_sizes('') == {})
+
+    
