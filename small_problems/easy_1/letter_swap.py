59b4c8beabbfdc5ff3cdeac7c63849fd7676032a
Joel Turske <joelturske@gmail.com>
2025-02-19 08:32:45 -0500

    Updated letter_swap.py, recreating LS solution.

--- small_problems/easy_1/letter_swap.py
+++ small_problems/easy_1/letter_swap.py
@@ -1,3 +1,6 @@
+'''First function is my first attempt at this problem. Commented out below is 
+a second attempt in which I recreated LS's solution to ensure I understood it.'''
+
 def swap(s):
     if len(s) == 1:
         return s
@@ -22,4 +25,21 @@ print(swap('Oh what a wonderful day it is')
       == "hO thaw a londerfuw yad ti si")  # True
 print(swap('Abcde') == "ebcdA")            # True
 print(swap('a') == "a")                    # True
+
+# def swap(s):
+#     words_list = s.split()
+#     for idx in range(len(words_list)):
+#         words_list[idx] = swap_chars(words_list[idx])
+
+#     return ' '.join(words_list)
+
+# def swap_chars(word):
+#     if len(word) == 1:
+#         return word
+    
+#     return word[-1] + word[1:-1] + word[0]
         
+# print(swap('Oh what a wonderful day it is')
+#       == "hO thaw a londerfuw yad ti si")  # True
+# print(swap('Abcde') == "ebcdA")            # True
+# print(swap('a') == "a")                    # True
