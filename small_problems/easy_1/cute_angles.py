fe60a6b3c4f25b3bb0b881a772736024e9eef310
Joel Turske <joelturske@gmail.com>
2025-02-21 12:21:25 -0500

    updated cute_angles.py

--- small_problems/easy_1/cute_angles.py
+++ small_problems/easy_1/cute_angles.py
@@ -1,38 +1,3 @@
-'''
-P & E-
-input: float between 0 and 360 (repr an angle)
-output: string (repr an angle) in degrees, minutes, and seconds
-Explicit:
-    - each of d,m, and s are followed by symbols (degree symbol, ', ")
-    - 60 minutes in a degree, 60 seconds in a minute
-    - 
-Implicit:
-    - need escape char for " after seconds in output string
-    - 0 input should return 0, 00, 00
-    - 360 input should return either 360, 00, 00 or 0, 00, 00
-    - if int input, minutes and seconds will be 00
-    - minutes and seconds must always contain two digits
-    - convert each degree, minute, second to int before interpolating
-    - whole number part of input float is the degree
-
-D-
-floats, ints, and strings
-f-strings for output
-
-A-
-Need quotients and integers (divmod?)
-Initialize DEGREE constant given in problem description
-Extract whole number from front of float assign to degree variable 
-    - divide by 1
-Take remainder, multiply by 60 and assign to minute variable 
-    - two steps above is divmod into two variables
-Take remainder, multiply by 60 and assign to second variable
-convert minutes and seconds to strings, check if they have two digits or one
-    - if only one digit, prepend a 0 to front of string
-return f-string with variables interpolated into appropriate positions
-convert to int before return?
-
-'''
 def ensure_two_digits(num):
     str_num = str(int(num))
     if len(str_num) == 1:
@@ -45,7 +10,7 @@ def dms(number):
     DEGREE = "\u00B0"
 
     if number < 0 or number > 360:
-        number = sanitize_number(number)
+        number %= 360
 
     degrees, remainder = divmod(number, 1)
     minutes = remainder * 60
@@ -64,36 +29,10 @@ print(dms(93.034773) == "93°02'05\"")
 print(dms(0) == "0°00'00\"")
 print(dms(360) == "360°00'00\"" or dms(360) == "0°00'00\"")
 
-'''Refactored for Further Exploration piece.
-P-
-input: any negative or positive number
-output: same as above
-
-A-
-additional math steps
-if outside the range of 0 - 360
-helper function:
-    if < 0
-        get quotient of input divided by -360
-        add input to 360 * quotient
-        Now we have a negative number between -360 and 0
-        subtract that number from 360
-        return number
-    if > 360
-        get quotient of input divided by 360
-        subtract 360 * quotient from input
-        return number
-'''
-def sanitize_number(number):
-    if number < 0:
-        quotient = number // -360
-        number += (360 * quotient) + 360
-        return number
-    if number > 360:
-        quotient = number // 360
-        return number - (360 * quotient)
-    
 print(dms(-1))   # 359°00'00"
 print(dms(400))  # 40°00'00"
 print(dms(-40))  # 320°00'00"
 print(dms(-420)) # 300°00'00"
+
+print(dms(-740)) # 340
+print(dms(-1200)) # 240
