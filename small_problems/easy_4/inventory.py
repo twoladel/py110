7703121c2013a784db68094745ccfd0dc213733a
Joel Turske <joelturske@gmail.com>
2025-03-27 18:24:55 -0400

    Another list comprehension solution. This time selecting dicts from a list of dicts that had a specfic value in the first key value pair.

---
+++ small_problems/easy_4/inventory.py
@@ -0,0 +1,25 @@
+def transactions_for(id_num, transactions):
+    return [
+        transaction for transaction in transactions
+        if transaction['id'] == id_num
+        ]
+
+transactions = [
+    {"id": 101, "movement": 'in',  "quantity":  5},
+    {"id": 105, "movement": 'in',  "quantity": 10},
+    {"id": 102, "movement": 'out', "quantity": 17},
+    {"id": 101, "movement": 'in',  "quantity": 12},
+    {"id": 103, "movement": 'out', "quantity": 20},
+    {"id": 102, "movement": 'out', "quantity": 15},
+    {"id": 105, "movement": 'in',  "quantity": 25},
+    {"id": 101, "movement": 'out', "quantity": 18},
+    {"id": 102, "movement": 'in',  "quantity": 22},
+    {"id": 103, "movement": 'out', "quantity": 15},
+]
+
+print(transactions_for(101, transactions) ==
+      [
+          {"id": 101, "movement": "in",  "quantity":  5},
+          {"id": 101, "movement": "in",  "quantity": 12},
+          {"id": 101, "movement": "out", "quantity": 18},
+      ]) # True
