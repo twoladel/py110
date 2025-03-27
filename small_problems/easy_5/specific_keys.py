a672ce5c603f583c5920e386abb139e75fbf4926
Joel Turske <joelturske@gmail.com>
2025-03-27 19:08:10 -0400

    Retain the dict items of only specific keys.

---
+++ small_problems/easy_5/specific_keys.py
@@ -0,0 +1,16 @@
+def keep_keys(dict, key_list):
+    return {
+        key: value for key, value in dict.items()
+        if key in key_list}
+
+
+input_dict = {
+    'red': 1,
+    'green': 2,
+    'blue': 3,
+    'yellow': 4,
+}
+
+keys = ['red', 'blue']
+expected_dict = {'red': 1, 'blue': 3}
+print(keep_keys(input_dict, keys) == expected_dict) # True
