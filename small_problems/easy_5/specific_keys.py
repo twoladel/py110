0d63fc3363b2d588c4220c327407549c88477aec
Joel Turske <joelturske@gmail.com>
2025-03-28 08:29:47 -0400

    Rewrote function to be more efficient. Explanation in docstring.

--- small_problems/easy_5/specific_keys.py
+++ small_problems/easy_5/specific_keys.py
@@ -1,9 +1,17 @@
+'''The commented out solution below was my original. After reviewing LS's 
+solution I realized that mine is less efficient because it iterates over the 
+entire dictionary instead of just the elements of the list of keys.'''
+# def keep_keys(dict, key_list):
+#     return {
+#         key: value for key, value in dict.items()
+#         if key in key_list
+#         }
+
 def keep_keys(dict, key_list):
     return {
-        key: value for key, value in dict.items()
-        if key in key_list
+        key: dict[key] for key in key_list
+        if key in dict
         }
-
 
 input_dict = {
     'red': 1,
