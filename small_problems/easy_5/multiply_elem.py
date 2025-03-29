57fe191974855eaa01588377b5b75792fcadfb3b
Joel Turske <joelturske@gmail.com>
2025-03-29 10:46:55 -0400

    added more pythonic solution with zip function.

--- small_problems/easy_5/multiply_elem.py
+++ small_problems/easy_5/multiply_elem.py
@@ -1,5 +1,8 @@
+# def multiply_items(lst1, lst2):
+#     return [lst1[idx] * lst2[idx] for idx in range(len(lst1))]
+
 def multiply_items(lst1, lst2):
-    return [lst1[idx] * lst2[idx] for idx in range(len(lst1))]
+    return [num1 * num2 for num1, num2 in zip(lst1, lst2)]
 
 list_a = [1, 2, 3]
 list_b = [4, 5, 6]
