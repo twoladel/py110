8348836278d87ae8f7f7b5351431c5c97493b259
Joel Turske <joelturske@gmail.com>
2025-03-29 11:09:51 -0400

    Sum the sums of subsequences of numbers in a list.

---
+++ small_problems/easy_5/sum_of_sums.py
@@ -0,0 +1,13 @@
+def sum_of_sums(lst):
+    return sum([sum(lst[:idx + 1]) for idx in range(len(lst))])
+
+print(sum_of_sums([3, 5, 2]) == 21)               # True
+# (3) + (3 + 5) + (3 + 5 + 2) --> 21
+
+print(sum_of_sums([1, 5, 7, 3]) == 36)            # True
+# (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+
+print(sum_of_sums([1, 2, 3, 4, 5]) == 35)         # True
+# (1) + (1+2) + (1+2+3) + (1+2+3+4) + (1+2+3+4+5) --> 35
+
+print(sum_of_sums([4]) == 4)                      # True
