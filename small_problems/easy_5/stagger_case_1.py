b33b648695530d12b9887301bf7cad0c58387587
Joel Turske <joelturske@gmail.com>
2025-03-30 10:10:56 -0400

    Stagger the case of each char in a string.

---
+++ small_problems/easy_5/stagger_case_1.py
@@ -0,0 +1,23 @@
+def staggered_case(string):
+    result = ''
+    for idx, char in enumerate(string):
+        if idx % 2 == 0:
+            result += char.upper() 
+        else: 
+            result += char.lower()
+
+    return result
+
+string = 'I Love Launch School!'
+result = "I LoVe lAuNcH ScHoOl!"
+print(staggered_case(string) == result)  # True
+
+string = 'ALL_CAPS'
+result = "AlL_CaPs"
+print(staggered_case(string) == result)  # True
+
+string = 'ignore 77 the 4444 numbers'
+result = "IgNoRe 77 ThE 4444 nUmBeRs"
+print(staggered_case(string) == result)  # True
+
+print(staggered_case('') == "")          # True
