b99d01902cf6c0d9b6c28410504234ae57e1a0ad
Joel Turske <joelturske@gmail.com>
2025-03-30 13:48:55 -0400

    Don't name a function the same thing as a built-in function.

---
+++ small_problems/debugging/shadow.py
@@ -0,0 +1,19 @@
+'''
+
+'''
+
+def mutltiply_sum(numbers, factor):
+    return factor * sum(numbers)
+
+numbers = [1, 2, 3, 4]
+print(mutltiply_sum(numbers, 2) == 20) # True
+
+'''Original code to refactor below. Example of why you shouldn't name your
+functions the same as already existing built-ins. Renamed the function
+above to handle the issue.'''
+
+def sum(numbers, factor):
+    return factor * sum(numbers)
+
+numbers = [1, 2, 3, 4]
+print(sum(numbers, 2) == 20) # False
