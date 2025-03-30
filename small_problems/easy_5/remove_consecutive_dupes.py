6351d4b68b548c67ac2f528ce8c43f0ea31ba693
Joel Turske <joelturske@gmail.com>
2025-03-30 11:51:20 -0400

    LS solution a variation on what I did so included it for reference.

--- small_problems/easy_5/remove_consecutive_dupes.py
+++ small_problems/easy_5/remove_consecutive_dupes.py
@@ -1,13 +1,26 @@
+# def unique_sequence(numbers):
+'''My solution. Similar to LS below.'''
+#     if not numbers:
+#         return []
+    
+#     result = [numbers[0]]
+#     for idx, num in enumerate(numbers[1:], start=1):
+#         if num != numbers[idx - 1]:
+#             result.append(num)
+
+#     return result
+
 def unique_sequence(numbers):
+    '''LS solution. Slightly different than my own.'''
     if not numbers:
         return []
-    
-    result = [numbers[0]]
-    for idx, num in enumerate(numbers[1:], start=1):
-        if num != numbers[idx - 1]:
-            result.append(num)
 
-    return result
+    unique = [numbers[0]]
+    for value in numbers[1:]:
+        if value != unique[-1]:
+            unique.append(value)
+
+    return unique
 
 
 original = [1, 1, 2, 6, 6, 6, 5, 5, 3, 3, 3, 4]
