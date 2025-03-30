dc98fe610589a8d6e7563fc2e0451ad5cb346572
Joel Turske <joelturske@gmail.com>
2025-03-30 16:57:31 -0400

    Rotating the a specific digit in a number to the rear. Convert to list, pop, append. Convert back.

---
+++ small_problems/medium_1/rotate_2.py
@@ -0,0 +1,14 @@
+def rotate_rightmost_digits(number, count):
+    nums = list(str(number))
+    last_digit = nums.pop(-count)
+    nums.append(last_digit)
+
+    return int(''.join(nums))
+
+print(rotate_rightmost_digits(735291, 2) == 735219)  # True
+print(rotate_rightmost_digits(735291, 3) == 735912)  # True
+print(rotate_rightmost_digits(735291, 1) == 735291)  # True
+print(rotate_rightmost_digits(735291, 4) == 732915)  # True
+print(rotate_rightmost_digits(735291, 5) == 752913)  # True
+print(rotate_rightmost_digits(735291, 6) == 352917)  # True
+print(rotate_rightmost_digits(1200, 3) == 1002)      # True
