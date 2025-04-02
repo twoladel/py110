fa9f51bc41ff4de294ed15fe4c5352c7a0869ad1
Joel Turske <joelturske@gmail.com>
2025-04-02 16:24:39 -0400

    refactored to a slightly better solution.

--- small_problems/medium_1/is_prime.py
+++ small_problems/medium_1/is_prime.py
@@ -2,14 +2,10 @@ def is_prime(number):
     if number == 1:
         return False
     
-    divisors = []
-
-    for num in range(1, number + 1):
+    for num in range(2, number):
         if number % num == 0:
-            divisors.append(num)
-
-            if len(divisors) > 2:
-                return False
+            return False
+        
     return True
 
 print(is_prime(1) == False)              # True
