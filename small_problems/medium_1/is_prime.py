0b558ba4504960866db66e5e774f66591678136d
Joel Turske <joelturske@gmail.com>
2025-04-02 16:23:45 -0400

    Determine if a number is prime without fancy bells and whistles.

---
+++ small_problems/medium_1/is_prime.py
@@ -0,0 +1,30 @@
+def is_prime(number):
+    if number == 1:
+        return False
+    
+    divisors = []
+
+    for num in range(1, number + 1):
+        if number % num == 0:
+            divisors.append(num)
+
+            if len(divisors) > 2:
+                return False
+    return True
+
+print(is_prime(1) == False)              # True
+print(is_prime(2) == True)               # True
+print(is_prime(3) == True)               # True
+print(is_prime(4) == False)              # True
+print(is_prime(5) == True)               # True
+print(is_prime(6) == False)              # True
+print(is_prime(7) == True)               # True
+print(is_prime(8) == False)              # True
+print(is_prime(9) == False)              # True
+print(is_prime(10) == False)             # True
+print(is_prime(23) == True)              # True
+print(is_prime(24) == False)             # True
+print(is_prime(997) == True)             # True
+print(is_prime(998) == False)            # True
+print(is_prime(3_297_061) == True)       # True
+print(is_prime(23_297_061) == False)     # True
