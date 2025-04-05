1c1e138555bcc2886989b89c75cdac345a3cfff2
Joel Turske <joelturske@gmail.com>
2025-04-05 10:40:57 -0400

    Find the next highest number after a given input that is an odd, mulitple of 7 and contains all unique digits.

---
+++ small_problems/medium_2/next_highest.py
@@ -0,0 +1,45 @@
+import time
+
+start_time = time.perf_counter()
+
+MAX_FEATURED = 9876543201
+
+def start_at_odd_mulitple_of_seven(number):
+    if number % 7 != 0:
+        start = number + 7 - (number % 7)
+    else:
+        start = number + 7
+
+    if start % 2 == 0:
+        start += 7
+
+    return start
+
+def next_featured(number):
+    if number >= MAX_FEATURED:
+        return 'There is no possible number that fulfills those requirements.'
+
+    start = start_at_odd_mulitple_of_seven(number)
+
+    for num in range(start, 9876543202, 14):
+        if len(str(num)) == len(set(str(num))):
+            return num
+
+
+print(next_featured(12) == 21)                  # True
+print(next_featured(20) == 21)                  # True
+print(next_featured(21) == 35)                  # True
+print(next_featured(997) == 1029)               # True
+print(next_featured(1029) == 1043)              # True
+print(next_featured(999999) == 1023547)         # True
+print(next_featured(999999987) == 1023456987)   # True
+print(next_featured(9876543186) == 9876543201)  # True
+print(next_featured(9876543200) == 9876543201)  # True
+
+error = ("There is no possible number that "
+         "fulfills those requirements.")
+print(next_featured(9876543201) == error)       # True
+
+end_time = time.perf_counter()
+elapsed = end_time - start_time
+print(f'Runtime: {elapsed:.4f}')
